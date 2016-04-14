20b374bf792e1286c322dcdcbb86361d0323e322
Romain Fontaine <romain-fontaine@outlook.com>
2016-04-14 15:50:32 +0200

    Index view

--- webview/documents/views.py
+++ webview/documents/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Document
 
 # Create your views here.
+class ListView(generic.ListView):
+    template_name = 'documents/index.html'
+    model = Document
+    context_object_name = 'documents_list'
