53b3d7daf5e89f7bb7767690cf46fa4f16f6272b
Romain Fontaine <romain-fontaine@outlook.com>
2016-04-14 15:04:13 +0200

    Add celery and sample tasks

--- webview/webview/settings.py
+++ webview/webview/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
