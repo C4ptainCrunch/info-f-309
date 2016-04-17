f72de7c4c6ca87c834ce353bc8d877af785b276c
Nikita Marchant <nikita.marchant@gmail.com>
2016-04-17 09:49:42 +0200

    Add clamav

--- webview/documents/tasks.py
+++ webview/documents/tasks.py
@@ -3,9 +3,30 @@ from __future__ import absolute_import
 from celery import shared_task
 
 from documents.models import Document
+import clamd
 
 
 @shared_task
 def compile_tex(document_id):
     document = Document.objects.get(pk=document_id)
     print(document)
+
+
+@shared_task
+def check_clamav(document_id):
+    document = Document.objects.get(pk=document_id)
+    clam = clamd.ClamdUnixSocket(settings.CLAMAV_SOCKET)
+    status, sig = clam.scan(absolute_path_to_pdf)[absolute_path_to_pdf]
+    if status == 'OK':
+        document.clean = True
+    elif status == 'FOUND':
+        document.clean = False
+        print("Signature found", sig)
+    else:
+        # unknown state
+       document.clean = False
+       print("Unknown return of clamav", status, sig)
+
+    document.save()
+
+
