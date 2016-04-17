687231db1d0933bfa0a8807ce7e955f5915bc966
Anthony Caccia <acaccia@ulb.ac.be>
2016-04-17 10:24:42 +0200

    jailifier draft

---
+++ scripts/jailifier.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Usage: jailifier.sh netid zip_archive
+
+J=/home/j
+JS=/home/js
+
+# creating jail locations
+mkdir "$J/$1"
+mkdir "$JS/$1"
+cpdup "$J/skel" "$JS/$1"
+
+# unzip and send sources to jail
+unzip "$2" -d "$JS/$1/home"
+
+# mounting jail
+mount_nullfs -o ro "$J/mroot" "$J/$1"
+mount_nullfs -o rw "$JS/$1" "$J/$1/s"
+mount -t devfs /dev "$J/$1/dev"
+
+# launching jail
+jail -c path="$J/$1" name="$1" persist
+
+# compiling latex + clamav + moving pdf output #TODO: path
+jexec "$1" sh -c 'cd /home && latexmk'
+clamscan "$JS/home/*.pdf"
+mv "$JS/home/*.pdf" ... #TODO
+
+# stopping jail + folder deletion
+jail -r "$1"
+
+umount "$J/$1/dev"
+umount "$J/"1/s"
+umount "$J/$1"
+chflags -R noschg "$JS/$1"
+chflags -R noschg "$J/$1"
+
+rm -rf "${JS/$1}"
+rm -rf "{$J/$1}"
+
