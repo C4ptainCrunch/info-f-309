1e580782a09ff67111d549fcf0d373d488650857
Romain Fontaine <romain-fontaine@outlook.com>
2016-04-17 16:22:01 +0200

    Subprocess call to run compilation command

--- webview/documents/tasks.py
+++ webview/documents/tasks.py
@@ -3,13 +3,18 @@ from __future__ import absolute_import
 from celery import shared_task
 
 from documents.models import Document
+from django.conf import settings
 import clamd
+import os
+import subprocess
 
 
 @shared_task
 def compile_tex(document_id):
+    script = os.path.join(settings.BASE_DIR, '../scripts', 'jailifier.sh')
     document = Document.objects.get(pk=document_id)
-    print(document)
+    command = ["sh", script, document.id, document.zipFile.path]
+    subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=1800, check=True)
 
 
 @shared_task
